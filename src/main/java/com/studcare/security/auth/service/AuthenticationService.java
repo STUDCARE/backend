25e4871d941a79a8604c659a167cc5825e1ac160
pwickramathunge <pwickramathunge@isa.ae>
2024-08-11 23:36:47 +0530

    STUDCARE-039 handling errors in login api

--- src/main/java/com/studcare/security/auth/service/AuthenticationService.java
+++ src/main/java/com/studcare/security/auth/service/AuthenticationService.java
@@ -1,16 +1,24 @@
 package com.studcare.security.auth.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.studcare.data.jpa.repository.UserRepository;
+import com.studcare.exception.StudCareRuntimeException;
+import com.studcare.exception.StudCareValidationException;
+import com.studcare.model.HttpRequestData;
+import com.studcare.model.HttpResponseData;
 import com.studcare.security.JwtService;
 import com.studcare.security.auth.dto.AuthenticationRequest;
 import com.studcare.security.auth.dto.AuthenticationResponse;
-import com.studcare.exception.StudCareValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-
 import org.springframework.stereotype.Service;
+
+import static com.studcare.util.CommonUtils.createResponseEntity;
 
 @Slf4j
 @Service
@@ -21,12 +29,56 @@ public class AuthenticationService {
 	private JwtService jwtService;
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	@Autowired
+	private ObjectMapper objectMapper;
 
-	public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
-		authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
-		var user = userRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow(() -> new StudCareValidationException("User not found"));
-		var jwtToken = jwtService.generateToken(user);
-		log.info("successfully login {}",user);
-		return AuthenticationResponse.builder().token(jwtToken).user(user).build();
+	public ResponseEntity<Object> authenticate(HttpRequestData httpRequestData) {
+		log.info("AuthenticationService.authenticate() initiated");
+		ResponseEntity<Object> responseEntity;
+		HttpResponseData httpResponseData = new HttpResponseData();
+		try {
+			AuthenticationRequest authenticationRequest = adaptAuthenticationRequest(httpRequestData);
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticationRequest.getEmail(), authenticationRequest.getPassword()));
+			var user = userRepository.findByEmail(authenticationRequest.getEmail()).orElseThrow(() -> new StudCareValidationException("User not found"));
+			var jwtToken = jwtService.generateToken(user);
+			AuthenticationResponse authenticationResponse = AuthenticationResponse.builder().token(jwtToken).user(user).build();
+			httpResponseData.setHttpStatus(HttpStatus.OK);
+			httpResponseData.setResponseBody(mapResponseData(authenticationResponse));
+			responseEntity = createResponseEntity(httpResponseData);
+			log.info("AuthenticationService.authenticate() finished for {}", authenticationRequest.getEmail());
+		} catch (StudCareValidationException exception) {
+			httpResponseData.setHttpStatus(HttpStatus.BAD_REQUEST);
+			httpResponseData.setResponseBody(exception.getMessage());
+			responseEntity = createResponseEntity(httpResponseData);
+			log.error("AuthenticationService.authenticate() a validation error occurred while processing the request", exception);
+		} catch (StudCareRuntimeException exception) {
+			httpResponseData.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+			httpResponseData.setResponseBody(exception.getMessage());
+			responseEntity = createResponseEntity(httpResponseData);
+			log.error("AuthenticationService.authenticate() a runtime error occurred while processing the request", exception);
+		} catch (Exception exception) {
+			httpResponseData.setHttpStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+			httpResponseData.setResponseBody(exception.getMessage());
+			responseEntity = createResponseEntity(httpResponseData);
+			log.error("AuthenticationService.authenticate() an error occurred while processing the request", exception);
+		}
+		return responseEntity;
+	}
+
+	private AuthenticationRequest adaptAuthenticationRequest(HttpRequestData httpRequestData) {
+		try {
+			return objectMapper.readValue(httpRequestData.getRequestBody(), AuthenticationRequest.class);
+		} catch (JsonProcessingException exception) {
+			log.error("AuthenticationService.adaptAuthenticationRequest(): map user register request to user object failed", exception);
+			throw new StudCareRuntimeException("user login request to AuthenticationRequest object failed");
+		}
+	}
+	private String mapResponseData(AuthenticationResponse authenticationResponse) {
+		try {
+			return objectMapper.writeValueAsString(authenticationResponse);
+		} catch (JsonProcessingException exception) {
+			log.error("AuthenticationService.adaptAuthenticationRequest(): map user register response to response object failed", exception);
+			throw new StudCareRuntimeException("user login response to AuthenticationResponse object failed");
+		}
 	}
 }
--- src/main/java/com/studcare/security/controller/AuthenticationController.java
+++ src/main/java/com/studcare/security/controller/AuthenticationController.java
@@ -1,15 +1,21 @@
 package com.studcare.security.controller;
 
-import com.studcare.security.auth.dto.AuthenticationRequest;
-import com.studcare.security.auth.dto.AuthenticationResponse;
+import com.studcare.model.HttpRequestData;
 import com.studcare.security.auth.service.AuthenticationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
@@ -17,8 +23,19 @@ public class AuthenticationController {
 	private AuthenticationService authenticationService;
 
 	@PostMapping("/login")
-	public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
-		return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+	public ResponseEntity<Object> authenticate(
+			@RequestHeader Map<String, String> headers,
+			@RequestParam Map<String, String> queryParams,
+			@RequestBody String requestBody
+	) {
+		try {
+			log.info("AccountController.authenticate()[POST] process initiated");
+			HttpRequestData httpRequestData = new HttpRequestData(headers, queryParams, requestBody);
+			return authenticationService.authenticate(httpRequestData);
+		} catch (Exception exception) {
+			log.error("AccountController.authenticate()[POST] unexpected error occurred", exception);
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
