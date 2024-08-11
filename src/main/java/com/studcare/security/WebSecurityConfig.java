06963ea81b0d898afdde55f5742dd6cf7b2ce85f
pwickramathunge <pwickramathunge@isa.ae>
2024-08-11 21:50:46 +0530

    STUDCARE-037 allowing cors policies

--- src/main/java/com/studcare/security/WebSecurityConfig.java
+++ src/main/java/com/studcare/security/WebSecurityConfig.java
@@ -46,17 +46,6 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("*"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
-
 }
 
 
